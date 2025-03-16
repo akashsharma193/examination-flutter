744f0b39c84fa1ada7b55668ecd577e83fa39bf7
AbhishekPrajapat23 <90974451+Abhishek00p@users.noreply.github.com>
2025-03-16 16:34:53 +0530

    chages made , secured app

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,28 +1,21 @@
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    fetch('/firebase-config.json') // Load config from a secure file
+      .then(response => response.json())
+      .then(config => {
+        importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
+        importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCPDruIF3PUNUemrXWVX_H1TECAll8dJec",
-  authDomain: "offline-14ce6.firebaseapp.com",
-  projectId: "offline-14ce6",
-  storageBucket: "offline-14ce6.firebasestorage.app",
-  messagingSenderId: "243295987001",
-  appId: "1:243295987001:web:2e6cce918237ee735174e8",
-  measurementId: "G-RTYXPH7TRY"
-});
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
+        firebase.initializeApp(config);
+        const messaging = firebase.messaging();
 
-/*messaging.onMessage((payload) => {
-console.log('Message received. ', payload);*/
-messaging.onBackgroundMessage(function (payload) {
-  console.log('Received background message ', payload);
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+        messaging.onBackgroundMessage(function (payload) {
+          console.log('Received background message: ', payload);
+          self.registration.showNotification(payload.notification.title, {
+            body: payload.notification.body,
+            icon: "/firebase-logo.png"
+          });
+        });
+      })
+  );
 });
