3c7c7280cdd7338c31b17106bb527df6f8c83551
Ajinkya998 <ajinkyakharujkar998@gmail.com>
2025-09-17 20:19:15 +0530

    Bug Fixes

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
+
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-6836526074800523/8692203491';
+      return 'ca-app-pub-6836526074800523/9549257078';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
@@ -13,18 +14,32 @@ class AdHelper {
   static String get testBannerAdUnitId {
     if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/6300978111';
-    }else {
+    } else {
       throw UnsupportedError('Unsupported platform');
     }
   }
 
-  static const bool useTestAds = true; //TODO: Set to false for production
+  static const bool useTestAds = false;
 
   static String getBannerAdUnitId() {
-    return useTestAds ? testBannerAdUnitId : bannerAdUnitId;
+    final adUnitId = useTestAds ? testBannerAdUnitId : bannerAdUnitId;
+    print('🎯 Ad Unit ID: $adUnitId');
+    print('🎯 Using ${useTestAds ? "TEST" : "PRODUCTION"} ads');
+    return adUnitId;
   }
+
+  // Add this method to check what type of ad is being used
+  static bool isUsingTestAds() => useTestAds;
+  static String getCurrentAdType() => useTestAds ? "TEST" : "PRODUCTION";
 
   static Future<void> initializeAds() async {
     await MobileAds.instance.initialize();
+    await MobileAds.instance.updateRequestConfiguration(
+      RequestConfiguration(
+        testDeviceIds: [],
+        tagForChildDirectedTreatment: TagForChildDirectedTreatment.unspecified,
+        tagForUnderAgeOfConsent: TagForUnderAgeOfConsent.unspecified,
+      ),
+    );
   }
 }
