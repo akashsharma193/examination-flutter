9f0331c27a39c3a2e4f5bbc3edb960ba4c04dce7
Ajinkya998 <ajinkyakharujkar998@gmail.com>
2025-09-18 18:43:03 +0530

    Origin Fixed

--- lib/data/remote/app_dio_service.dart
+++ lib/data/remote/app_dio_service.dart
@@ -54,7 +54,7 @@ class AppDioService {
           'Content-Type': 'application/json',
           'deviceId': await DeviceService.instance.uniqueDeviceId,
           'encDisabled': 'false',
-          'Origin': 'https://crackitx.contentive.in',
+          'Origin': 'https://examdy.in',
         },
         connectTimeout: const Duration(minutes: 2),
         sendTimeout: const Duration(minutes: 2),
